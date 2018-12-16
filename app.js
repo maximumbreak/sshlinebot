d12438ed19f4144252de235dff6ddd699cf113c6
first <firstnaruto@msn.com>
2018-12-16 19:54:24 +0700

    [+] Test line bot

--- app.js
+++ app.js
@@ -17,5 +17,40 @@ app.listen(app.get('port'), function() {
 })
 
 app.post('/webhook', (req, res) => {
+  var text = req.body.events[0].message.text
+  var sender = req.body.events[0].source.userId
+  var replyToken = req.body.events[0].replyToken
+  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+    sendText(sender, text)
+  }
   res.sendStatus(200)
 })
+
+function sendText(sender, text) {
+  let data = {
+    to: sender,
+    messages: [
+      {
+        type: 'text',
+        text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞'
+      }
+    ]
+  }
+  request(
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer key Api'
+      },
+      url: 'https://api.line.me/v2/bot/message/push',
+      method: 'POST',
+      body: data,
+      json: true
+    },
+    function(err, res, body) {
+      if (err) console.log('error')
+      if (res) console.log('success')
+      if (body) console.log(body)
+    }
+  )
+}
